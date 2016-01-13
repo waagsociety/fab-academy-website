c622bce20418b0f3188e0771dd79553a9751b064
Martin Risseeuw <hello@martinr.nl>
2016-01-13 16:08:36 +0100

    Updated sag animation

--- build/public/js/main.js
+++ build/public/js/main.js
@@ -1,9 +1,29 @@
-function init(){
-  stickyMenu();
-}
+var btn = document.getElementById('viewProjectBtn'),
+	rect = btn.getBoundingClientRect();
+console.log(rect.top, rect.right, rect.bottom, rect.left);
 
-window.onresize=function(){
-  stickyMenu();
-}
 
-init();
+btn.addEventListener("click", openBtn);
+
+function openBtn(){
+	var page = document.querySelector('.page');
+
+	btn.style.width = btn.offsetWidth + "px";
+	btn.style.position = "fixed";
+	btn.style.left = rect.left - window.scrollX + "px";
+	btn.style.top = rect.top - window.scrollY - 50 + "px";
+	document.body.style.overflowY = "hidden";	
+
+	setTimeout(function(){ 
+		btn.innerHTML = "";
+		btn.classList.add("open"); 
+	}, 300);
+
+	setTimeout(function(){ 
+		page.classList.add("open");
+	}, 350);
+
+	setTimeout(function(){ 
+		page.style.opacity = 1;
+	}, 500);
+}
